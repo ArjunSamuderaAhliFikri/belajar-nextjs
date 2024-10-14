8b881f0937841506a56dc39a02e1b302d6c9b8c7
ArjunSamuderaAhliFikri <arjunsaf717@gmail.com>
2024-10-14 09:59:59 +0700

    initial commit - working with nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "../app/styles/globals.css";
+import { inter } from "./fonts/primaryFont";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <div className="text-slate-700 capitalize font-semibold text-2xl"></div>
+    </>
+  );
+}
