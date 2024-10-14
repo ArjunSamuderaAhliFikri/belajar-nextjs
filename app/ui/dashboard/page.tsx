27d1d2954d4e366a7e18c7a7a5617b0809931e1d
ArjunSamuderaAhliFikri <arjunsaf717@gmail.com>
2024-10-14 10:31:41 +0700

    error handling

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <>
-      <div className="text-slate-700 capitalize font-semibold text-2xl"></div>
+      <div className="text-slate-700 capitalize font-semibold text-2xl">
+        <h1>hello page!</h1>
+      </div>
     </>
   );
 }
